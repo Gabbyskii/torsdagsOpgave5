0cb3f04b485952f9ee22e2b6b70daa33ff4d0f43
Gabbyskii <gkadiga1@gmail.com>
2025-09-24 22:43:19 +0200

    task4 start - not done yet

---
+++ task4/GameMenu.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+//4.a - class GameMenu
+class GameMenu {
+    //4.b - arraylist instance
+    private ArrayList<String> actions;
+    
+    public static void main(String[]args){
+        
+    }
+}
